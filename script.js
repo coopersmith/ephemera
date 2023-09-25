999b4770b9762663254a0b6c1815114d3bc8d717
Cooper Smith <coopersmi@gmail.com>
2023-09-25 09:48:37 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// Replace with your own values
+const API_URL = 'https://api.airtable.com/v0/appSDh51MiE2vHKEZ/tbl6Y7weXgHGBUO3S';
+const API_KEY = 'key0aOgJtjQGR65d3';
+
+fetch(API_URL, {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const records = data.records;
+    const gallery = document.getElementById('gallery');
+  
+    records.forEach(record => {
+      if (record.fields.images && record.fields.images.length > 0) {
+        const imageUrl = record.fields.images[0].url;
+        const venue = record.fields.venue || 'Unknown Venue';
+        const date = record.fields.date || 'Unknown Date';
+  
+        const imgWrapper = document.createElement('div');
+        imgWrapper.className = 'img-wrapper';
+  
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
+  
+        const infoOverlay = document.createElement('div');
+        infoOverlay.className = 'info-overlay';
+  
+        const venueElement = document.createElement('span');
+        venueElement.textContent = `Venue: ${venue}`;
+  
+        const dateElement = document.createElement('span');
+        dateElement.textContent = `Date: ${date}`;
+  
+        infoOverlay.appendChild(venueElement);
+        infoOverlay.appendChild(dateElement);
+  
+        imgWrapper.appendChild(imgElement);
+        imgWrapper.appendChild(infoOverlay);
+        gallery.appendChild(imgWrapper);
+      } else {
+        console.warn('Image not found for record:', record.id);
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
